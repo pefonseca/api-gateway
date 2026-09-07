7091162c5b6a184ee0f8ae705c81fa205ac67555
Pedro Fonseca <pedrofonunes@gmail.com>
2026-09-07 13:35:04 -0300

    feat: ajustando ApiGateway

--- src/main/java/com/pefonseca/api/gateway/config/ApiGatewayConfiguration.java
+++ src/main/java/com/pefonseca/api/gateway/config/ApiGatewayConfiguration.java
@@ -11,11 +11,8 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/get")
-                        .filters(f -> f
-                                .addRequestHeader("Hello", "World")
-                                .addRequestParameter("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/book-service/**").uri("lb://book-service"))
+                .route(p -> p.path("/exchange-service/**").uri("lb://exchange-service"))
                 .build();
 
     }
